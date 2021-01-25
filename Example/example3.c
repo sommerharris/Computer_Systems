35447ca9b61f0c400456f133c3b75c6b179e9d42
Sommer Harris <sommerharris@login-students.ccs.neu.edu>
2021-01-25 15:21:39 -0500

    "This is my commit after watching Lesson 11: Introduction to Functions in C".

---
+++ Example/example3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	int array[3];
+	array[0] = 0;
+	array[1] = 1;
+	array[2] = 2;
+
+	for ( int i = 0; i < 3; i++){
+		printf("%d\n", array[i]);
+
+	}
+	return 0;
+}
