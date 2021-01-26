53ec874be9909c90488cf107a34af37b0aa92364
Sommer Harris <sommerharris@login-students.ccs.neu.edu>
2021-01-26 15:52:10 -0500

    Now it compiles AND runs.

--- military_time.c
+++ military_time.c
@@ -1,3 +1,6 @@
+#include <math.h>
+#include <stdio.h>
+
 int timeDifference(int arg1, int arg2){
 	return arg2 - arg1;
 }	
@@ -5,8 +8,9 @@ int main(){
 	int difference = timeDifference(1000, 1200);
 	double rawHours = floor(difference / 100); // 100; //time needs to be written as four consecutive digits	
 	double rawMinutes = difference - floor(difference / 100) * 100;
+	int intRawMinutes = (int)rawMinutes;
 	double hours = rawHours + floor(rawMinutes / 60);
-	double minutes = rawMinutes % 60;
+	int minutes = intRawMinutes % 60;
 
-	printf("Hours:, %f, Minutes:, %f\n", hours, minutes)
+	printf("Hours:, %f, Minutes:, %d\n", hours, minutes);
 }
